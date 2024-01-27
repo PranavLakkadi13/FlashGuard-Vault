11977032fa2a21a1318ffc3b3af0e597a4344808
Pranav <pranavlakkadi@gmail.com>
2024-01-27 17:56:55 +0530

    chore: forge init

--- Backend/test/TestFactory.t.sol
+++ Backend/test/TestFactory.t.sol
@@ -1,17 +1,17 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.0;
+// // SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// pragma solidity ^0.8.0;
 
-import {Test} from "forge-std/Test.sol";
-import {Factory} from "../src/VaultContract.sol";
-contract TestFactory is Test {
+// import {Test} from "forge-std/Test.sol";
+// import {Factory} from "../src/VaultContract.sol";
+// contract TestFactory is Test {
 
-    Factory public factory;
+//     Factory public factory;
 
-    function setUp() public {
-        factory = new Factory();
-    }
+//     function setUp() public {
+//         factory = new Factory();
+//     }
 
-    function testCreateVault() public {
-        factory.createVault(address(mockBtc),100,address(0));
-    }
-}
+//     function testCreateVault() public {
+//         factory.createVault(address(mockBtc),100,address(0));
+//     }
+// }
--- Backend/test/TestVault.t.sol
+++ Backend/test/TestVault.t.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.19;
 
-<<<<<<< HEAD
-import {Test} from ""
-=======
 import {Test} from "forge-std/Test.sol";
 import {Factory} from "../src/Factory.sol";
 
@@ -21,4 +18,3 @@ contract TestVault is Test {
         factory.createVault(address(0x1), 100, address(0));
     }
 }
->>>>>>> f45df2ffac8cd785bd38d1b8247c99f389750d24
